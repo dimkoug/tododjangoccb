158f008dd545fee83245f1f741b88f3f0b621ddf
Dimitris Kougioumtzis <dimkoug@gmail.com>
2022-02-01 13:26:40 +0200

    add django extensions, django debug toolbar for development

--- tododjangoccbv/tododjangoccbv/settings_local_sample.py
+++ tododjangoccbv/tododjangoccbv/settings_local_sample.py
@@ -4,7 +4,12 @@ from .settings_base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
+INTERNAL_IPS = ('127.0.0.1', '0.0.0.0')
 
+INSTALLED_APPS += (
+    'debug_toolbar',
+    'django_extensions',
+)
 
 DATABASES = {
     'default': {
@@ -12,6 +17,30 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel'
+]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True,
+    'EXTRA_SIGNALS': [],
+}
+
+MIDDLEWARE += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
 
 AUTH_PASSWORD_VALIDATORS = []
 
--- tododjangoccbv/tododjangoccbv/urls.py
+++ tododjangoccbv/tododjangoccbv/urls.py
@@ -28,3 +28,10 @@ if settings.DEBUG:
         settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    try:
+        import debug_toolbar
+        urlpatterns += [
+            path('__debug__', include(debug_toolbar.urls)),
+        ]
+    except ImportError:
+        pass
